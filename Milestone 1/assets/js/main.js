b52fc6671cefb8f82ba017d26eb17daf2c26a527
matteobuscicchio <matteoezio@me.com>
2021-01-12 19:23:02 +0100

    aggiunto percorso con chiave api

--- Milestone 1/assets/js/main.js
+++ Milestone 1/assets/js/main.js
@@ -1,13 +1,18 @@
 // Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo
-// scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
-// bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
+// Creare un layout base con una searchbar (una input e un button) in cui 
+// possiamo scrivere completamente o parzialmente il nome di un film. 
+// Possiamo, cliccando il bottone, cercare sull’API tutti 
+// i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo 
+// i seguenti valori per ogni
 // film trovato:
 // 1. Titolo
 // 2. Titolo Originale
 // 3. Lingua
 // 4. Voto
+
+// apiKey
+// https://api.themoviedb.org/3/movie/550?api_key=5c002e8033723e03762798df6a4b2e57
 
 let app = new Vue({
     el: '#app',
